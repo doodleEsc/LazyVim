c475136ac299fc832c621f68df2ffb5cbb9069ba
fanlizhou <fanlizhou@yunqilaohe.com>
2024-09-06 10:38:26 +0800

    feat: enable plugin functionality on VeryLazy event
    - Add an option to enable the functionality in the configuration
    - Modify the plugin to trigger on a "VeryLazy" event

--- lua/lazyvim/plugins/extras/coding/zhim.lua
+++ lua/lazyvim/plugins/extras/coding/zhim.lua
@@ -4,6 +4,7 @@ return {
     lazy = true,
     event = "VeryLazy",
     opts = {
+      enabled = true,
 
       -- im-select binary's name, or the binary's full path
       command = { "im-select.exe" },
