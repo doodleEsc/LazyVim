a212d9cf1d3a32b6c623d1b2de4d0f855e489a99
范立洲 <fanlizhou@yunqilaohe.com>
2024-12-17 18:57:05 +0800

    feat: add new plugin configurations and utility enhancements
    - Add a new plugin configuration for Avante, including options for OpenAI integration and system prompts.
    - Introduce a new plugin configuration for Zhim, which manages input method changes.
    - Create a new plugin configuration for Neoscroll, enabling smooth scrolling with customizable key mappings.
    - Add an optional plugin configuration for Outline, allowing for symbol folding and preview window settings.
    - Introduce a new plugin configuration for Alpha, setting up a dashboard with custom buttons and layout.
    - Add a new plugin configuration for Gruvbox, including color settings and integration with bufferline.
    - Update Lualine configuration to improve color handling and remove unnecessary options.
    - Introduce a new utility for loading environment variables from a .env file.
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

---
+++ lua/lazyvim/plugins/extras/ai/avante.lua
@@ -0,0 +1,142 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = true,
+    init = function()
+      LazyVim.env.load()
+    end,
+    version = false, -- set this if you want to always pull the latest change
+    build = "make",
+    opts = {
+      ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+      provider = "openai", -- Recommend using Claude
+      auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+      ---@alias Tokenizer "tiktoken" | "hf"
+      -- Used for counting tokens and encoding text.
+      -- By default, we will use tiktoken.
+      -- For most providers that we support we will determine this automatically.
+      -- If you wish to use a given implementation, then you can override it here.
+      tokenizer = "tiktoken",
+      ---@alias AvanteSystemPrompt string
+      -- Default system prompt. Users can override this with their own prompt
+      -- You can use `require('avante.config').override({system_prompt = "MY_SYSTEM_PROMPT"}) conditionally
+      -- in your own autocmds to do it per directory, or that fit your needs.
+      system_prompt = [[
+You are an excellent programming expert.All responses are in Simplified Chinese.
+]],
+      openai = {
+        endpoint = LazyVim.env.get("OPENAI_BASE_URL"),
+        model = "gpt-4o",
+        max_tokens = 4096,
+        timeout = 30000, -- Timeout in milliseconds
+        temperature = 0,
+        ["local"] = false,
+      },
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = true,
+      },
+      mappings = {
+        --- @class AvanteConflictMappings
+        diff = {
+          ours = "co",
+          theirs = "ct",
+          all_theirs = "ca",
+          both = "cb",
+          cursor = "cc",
+          next = "]x",
+          prev = "[x",
+        },
+        suggestion = {
+          accept = "<M-CR>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+        jump = {
+          next = "]]",
+          prev = "[[",
+        },
+        submit = {
+          normal = "<CR>",
+          insert = "<M-CR>",
+        },
+        sidebar = {
+          switch_windows = "<Tab>",
+          reverse_switch_windows = "<S-Tab>",
+        },
+      },
+      hints = { enabled = true },
+      windows = {
+        ---@type "right" | "left" | "top" | "bottom"
+        position = "right", -- the position of the sidebar
+        wrap = true, -- similar to vim.o.wrap
+        width = 30, -- default % based on available width
+        sidebar_header = {
+          align = "center", -- left, center, right for title
+          rounded = true,
+        },
+      },
+      highlights = {
+        ---@type AvanteConflictHighlights
+        diff = {
+          current = "DiffText",
+          incoming = "DiffAdd",
+        },
+      },
+      --- @class AvanteConflictUserConfig
+      diff = {
+        autojump = true,
+        ---@type string | fun(): any
+        list_opener = "copen",
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        lazy = true,
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    config = function(_, opts)
+      require("avante_lib").load()
+      local avante = require("avante")
+      avante.setup(opts)
+    end,
+  },
+}
