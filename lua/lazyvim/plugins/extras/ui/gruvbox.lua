e75974f7efee5790f74ac6d21d87cb1ec0cadb89
范立洲 <fanlizhou@yunqilaohe.com>
2024-12-17 18:57:05 +0800

    feat: add new plugin configurations and utility enhancements
    - Add a new plugin configuration for Avante, including options for OpenAI integration and system prompts.
    - Introduce a new plugin configuration for Zhim, which manages input method changes.
    - Create a new plugin configuration for Neoscroll, enabling smooth scrolling with customizable key mappings.
    - Add an optional plugin configuration for Outline, allowing for symbol folding and preview window settings.
    - Introduce a new plugin configuration for Alpha, setting up a dashboard with custom buttons and layout.
    - Add a new plugin configuration for Gruvbox, including color settings and integration with bufferline.
    - Update Lualine configuration to improve color handling and remove unnecessary options.
    - Introduce a new utility for loading environment variables from a .env file.
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

---
+++ lua/lazyvim/plugins/extras/ui/gruvbox.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {
+        SignColumn = { bg = "NONE" },
+      },
+      dim_inactive = false,
+      transparent_mode = false,
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
+  },
+}
