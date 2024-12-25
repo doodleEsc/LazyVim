26087aa9207d8e7c871e7b62eb551f88e5be02fd
doodleEsc <cinuor@gmail.com>
2024-12-25 18:12:50 +0800

    feat: configure Telescope plugin settings and layout options
    - Add a new configuration file for Telescope plugin settings
    - Set various layout and sorting options for Telescope in the configuration
    
    Signed-off-by: doodleEsc <cinuor@gmail.com>

---
+++ lua/lazyvim/plugins/extras/editor/telescope-config.lua
@@ -0,0 +1,29 @@
+return {
+
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        sorting_strategy = "ascending",
+        scroll_strategy = "cycle",
+        layout_strategy = "flex",
+        layout_config = {
+          horizontal = {
+            width = 0.9,
+            height = 0.9,
+            preview_cutoff = 120,
+            preview_width = 0.45,
+            prompt_position = "top",
+          },
+          vertical = {
+            height = 0.9,
+            width = 0.9,
+            preview_cutoff = 40,
+            prompt_position = "top",
+          },
+        },
+      })
+    end,
+  },
+}
