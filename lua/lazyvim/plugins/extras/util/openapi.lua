1a877332b7f69b2f77f9d677c14ff7d5934c3c8d
范立洲 <fanlizhou@yunqilaohe.com>
2025-01-19 11:51:35 +0800

    docs: refactor API configuration and update file paths
    - Update the configuration in `openapi.lua` to include port and host information
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

--- lua/lazyvim/plugins/extras/util/openapi.lua
+++ lua/lazyvim/plugins/extras/util/openapi.lua
@@ -3,6 +3,9 @@ return {
     "lucasrabiec/swagger-preview.nvim",
     cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
     build = "npm i",
-    config = true,
+    config = {
+      port = 12888,
+      host = "0.0.0.0",
+    },
   },
 }
