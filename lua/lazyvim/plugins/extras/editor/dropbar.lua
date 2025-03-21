e687c920052b73775031ba33908af4947370be1c
范立洲 <fanlizhou@yunqilaohe.com>
2025-03-21 11:20:40 +0800

    feat: consolidate dropbar functionality configurations
    - Add a new Lua file `dropbar.lua` with key mappings and configurations for dropbar functionality
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

---
+++ lua/lazyvim/plugins/extras/editor/dropbar.lua
@@ -0,0 +1,25 @@
+local build_cmd ---@type string?
+for _, cmd in ipairs({ "make", "cmake", "gmake" }) do
+  if vim.fn.executable(cmd) == 1 then
+    build_cmd = cmd
+    break
+  end
+end
+
+return {
+  {
+    "Bekaboo/dropbar.nvim",
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = (build_cmd ~= "cmake") and "make"
+        or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    },
+    config = function()
+      local dropbar_api = require("dropbar.api")
+      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+      vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+    end,
+  },
+}
