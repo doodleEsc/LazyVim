6c5b82f542cbf2c534d4a8fecdee4743e0f264ef
范立洲 <fanlizhou@yunqilaohe.com>
2025-03-21 11:35:59 +0800

    refactor: move none-ls configuration to custom utility extras
    Here's the summary of the git diff:
    
    - Remove `none-ls-config.lua` from LSP extras
    - Move none-ls configuration to the custom utility extras
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

--- lua/lazyvim/plugins/extras/util/custom.lua
+++ lua/lazyvim/plugins/extras/util/custom.lua
@@ -340,4 +340,18 @@ return {
       return opts
     end,
   },
+
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = {
+        nls.builtins.formatting.shfmt,
+        nls.builtins.code_actions.gitsigns,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.prettier,
+      }
+    end,
+  },
 }
