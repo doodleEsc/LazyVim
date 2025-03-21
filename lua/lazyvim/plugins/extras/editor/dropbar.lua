3df32b43281b4167667b7ccd1cb22877f53fcbcd
范立洲 <fanlizhou@yunqilaohe.com>
2025-03-21 11:29:45 +0800

    feat: consolidate configuration changes across files
    - Add event `VeryLazy` to `dropbar.lua`
    - Remove `outline-config.lua` file
    - Duplicate configuration from `custom.lua` to `dropbar.lua`
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

--- lua/lazyvim/plugins/extras/editor/dropbar.lua
+++ lua/lazyvim/plugins/extras/editor/dropbar.lua
@@ -9,6 +9,7 @@ end
 return {
   {
     "Bekaboo/dropbar.nvim",
+    event = "VeryLazy",
     -- optional, but required for fuzzy finder support
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
