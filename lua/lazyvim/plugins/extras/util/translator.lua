fabd7d05196baac29a97dc01a84bac4e16c49b98
范立洲 <fanlizhou@yunqilaohe.com>
2025-03-28 17:42:09 +0800

    feat: update translator plugin integration for new features and UI improvements
    - Add a new translator.lua file for the `doodleEsc/translator.nvim` plugin.
    - Define key bindings for translating text in visual mode.
    - Set default source language to "auto" and target language to "zh" for translation.
    - Configure translation options including prompt format, streaming, and engines.
    - Define UI settings for translator plugin, including border styling and text alignment.
    
    Signed-off-by: 范立洲 <fanlizhou@yunqilaohe.com>

---
+++ lua/lazyvim/plugins/extras/util/translator.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "doodleEsc/translator.nvim",
+    dev = true,
+    event = "VeryLazy",
+    keys = {
+      { "<leader>ts", ":Translate<CR>", mode = "v", desc = "Translate" },
+    },
+    opts = {
+      source_language = "auto",
+      target_language = "zh",
+      prompt = "Translate the following text from $SOURCE_LANG to $TARGET_LANG, no explanations.:\n```$TEXT\n```",
+      streaming = true,
+      proxy = nil,
+      translate_engine = {
+        base_url = "https://openrouter.ai/api/v1",
+        model = "qwen/qwen-turbo",
+      },
+      detect_engine = {
+        base_url = "https://openrouter.ai/api/v1",
+        model = "meta-llama/llama-3.2-3b-instruct",
+      },
+      ui = {
+        width = 0.6,
+        height = 0.3,
+        border = {
+          style = "double",
+          text = {
+            top_source = " 原文 ",
+            top_target = " 译文 ",
+            top_align = "left",
+          },
+        },
+      },
+      keymaps = {
+        enable = false,
+        translate = "<leader>ts",
+      },
+    },
+  },
+}
