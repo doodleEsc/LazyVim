5c525e18268e6381b484df6096b0b54250bbf55a
doodleEsc <cokie@foxmail.com>
2025-05-26 18:47:58 +0800

    refactor: remove chat strategy's dependency on the `mcp` tool.
    - Remove the `mcp` tool from the chat strategy.

--- lua/lazyvim/plugins/extras/ai/codecompanion.lua
+++ lua/lazyvim/plugins/extras/ai/codecompanion.lua
@@ -56,15 +56,6 @@ return {
         strategies = {
           chat = {
             adapter = "openrouter",
-            tools = {
-              ["mcp"] = {
-                -- calling it in a function would prevent mcphub from being loaded before it's needed
-                callback = function()
-                  return require("mcphub.extensions.codecompanion")
-                end,
-                description = "Call tools and resources from the MCP Servers",
-              },
-            },
           },
           inline = {
             adapter = "openrouter",
