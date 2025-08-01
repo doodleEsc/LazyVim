dca2a7d2494b6af1ca2a26db79b41f1ac5987cdd
nothankyou <luxun070188@outlook.com>
2025-08-02 00:02:22 +0800

    fix(venv-selector): incorrect configuration

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -117,10 +117,8 @@ return {
     branch = "regexp", -- Use this branch for the new version
     cmd = "VenvSelect",
     opts = {
-      settings = {
-        options = {
-          notify_user_on_venv_activation = true,
-        },
+      options = {
+        notify_user_on_venv_activation = true,
       },
     },
     --  Call config for python files and load the cached venv automatically
