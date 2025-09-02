9acd0e933af21e0e9f13055cf554d1d890a3bc8f
you-n-g <you-n-g@users.noreply.github.com>
2025-09-02 10:37:55 +0800

    chore: unify VenvSelect branch with main
    - Update the VenvSelect branch to `main`

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -114,6 +114,7 @@ return {
 
   {
     "linux-cultist/venv-selector.nvim",
+    branch = "main", -- Important: VenvSelect is now using `main` as the updated branch again.
     cmd = "VenvSelect",
     opts = {
       options = {
